ca27b6f099e2be700ae2c8e76f97d07dc5db40ab
Richard McElhinney <richard.mcelhinney@conserveitiot.com>
2022-12-02 22:37:42 +1100

    Day 1 - computed answer to part 1

--- src/test/java/com/conserveit/aoc/ElfModelTest.java
+++ src/test/java/com/conserveit/aoc/ElfModelTest.java
@@ -4,13 +4,42 @@ import com.conserveit.aoc.day1.ElfFactory;
 import org.testng.annotations.*;
 import org.testng.*;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
 import java.util.*;
 
 @Test
 public class ElfModelTest
 {
+    @Test
+    public void solveDay1Input() throws IOException, URISyntaxException
+    {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("day1.txt").getFile());
+        String data = new String(Files.readAllBytes(file.toPath()), "UTF-8");
+
+        StringReader rdr = new StringReader(data);
+        BufferedReader buf = new BufferedReader(rdr);
+
+        System.out.println("Day 1");
+        System.out.println("------");
+        try
+        {
+            List<ElfModel> elves = ElfFactory.createElves(buf);
+            Collections.sort(elves, new ElfSorter());
+            Collections.reverse(elves);
+
+            System.out.println(elves.get(0).getName() + " has the most food\n\n");
+
+            elves.forEach(e -> System.out.println(e.toString()));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void findElfWithMostFood()
     {
